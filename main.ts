726489a765be338dd561e32c4e10ba75b0cefe41
amrohan <amrohanx@gmail.com>
2023-10-09 09:57:12 +0530

    optional chaining added

--- main.ts
+++ main.ts
@@ -11,40 +11,33 @@ import * as timezoneController from './controllers/timezoneController';
 import * as userController from './controllers/userController';
 
 const app = express();
-app.use(bodyParser.json(), cookieParser(), cors());
+app.use(bodyParser.json(), cookieParser(), cors(), (req, res, next) => {
+    console.log(`${req.method} - ${req.url} ${req.hostname}`);
+    next();
+});
 
 
 // Users
-app.get('/users', userController.getAllUsers)
-app.get('/users/:id', userController.getUserById)
-app.post('/users', userController.createUser)
-app.put('/users/:id', userController.updateUserById)
-app.delete('/users/:id', userController.deleteUserById)
-
+app.route('/users').get(userController.getAllUsers).post(userController.createUser)
+app.route('/users/:id').get(userController.getUserById).put(userController.updateUserById).delete(userController.deleteUserById)
 
 // transactions
-app.get('/transactions', transactionController.all)
+app.route('/transactions').get(transactionController.all).post(transactionController.create)
+app.route('/transactions/:id').put(transactionController.updateByTransactionId).delete(transactionController.deleteByTransactionId)
 app.get('/transactions/:userId', transactionController.getTransactionsByUserId)
 app.get('/transactions/user/:userId', transactionController.getTransactionByMonthandYear)
-app.post('/transactions', transactionController.create)
-app.put('/transactions/:id', transactionController.updateByTransactionId)
-app.delete('/transactions/:id', transactionController.deleteByTransactionId)
+
 
 // Accounts 
-app.get('/accounts', accountController.getAllAccounts)
-app.get('/accounts/:id', accountController.getAccountById)
+app.route('/accounts').get(accountController.getAllAccounts).post(accountController.createAccount)
 app.get('/accounts/user/:userId', accountController.getAccountsByUserId)
-app.post('/accounts', accountController.createAccount)
-app.put('/accounts/:id', accountController.updateAccountById)
-app.delete('/accounts/:id', accountController.deleteAccountById)
+app.route('/accounts/:id').get(accountController.getAccountById).put(accountController.updateAccountById).delete(accountController.deleteAccountById)
+
 
 // Categories
-app.get('/categories', categoryController.getAllCategories)
-app.get('/categories/:id', categoryController.getCategoryById)
+app.route('/categories').get(categoryController.getAllCategories).post(categoryController.createCategory)
 app.get('/categories/user/:userId', categoryController.getCategoriesByUserId)
-app.post('/categories', categoryController.createCategory)
-app.put('/categories/:id', categoryController.updateCategoryById)
-app.delete('/categories/:id', categoryController.deleteCategoryById)
+app.route('/categories/:id').get(categoryController.getCategoryById).put(categoryController.updateCategoryById).delete(categoryController.deleteCategoryById)
 
 // Timezone
 app.get('/timezone', timezoneController.getTimeZone)
